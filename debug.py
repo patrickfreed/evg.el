f7c916a941cf8de221b5fa6f0a8b991bb9b21387
Patrick Freed <patrick.freed@mongodb.com>
2020-10-28 18:22:52 -0400

    navigate according to index, wip graphql

--- debug.py
+++ debug.py
@@ -6,15 +6,23 @@ import json
 patch_id = uuid.uuid4()
 headers = {
     'Api-User': os.environ['EVG_API_USER'],
-    'Api-Key': os.environ['EVG_API_KEY']
+    'Api-Key': os.environ['EVG_API_KEY'],
+    'Content-Type': 'application/json'
 }
 
-url = "https://evergreen.mongodb.com/api/rest/v2/projects/{}/patches".format("mongo-rust-driver")
-data = {
-    'limit': 10
-}
-result = requests.get(url, headers=headers, data=data)
+graphql_url = "https://evergreen.mongodb.com/graphql/query"
+url = "https://evergreen.mongodb.com/api/rest/v2/patches/{}".format("5f98b24fd6d80a586d0d1288")
+# data = {
+#     'limit': 10
+# }
+# result = requests.get(url, headers=headers)
+# print(result.status_code)
+# results = result.json()
+# sample = { k: results[k] for k in results # if k != "patched_config"
+# }
+# print(json.dumps(sample, indent=4))
+
+data = { 'query': 'query { patch(id: "5f98b24fd6d80a586d0d1288") { project { variants { displayName,tasks }}}}'}
+result = requests.post(graphql_url, headers=headers, data=json.dumps(data))
 print(result.status_code)
-results = result.json()[0]
-sample = { k: results[k] for k in results if k != "patched_config" }
-print(json.dumps(sample, indent=4))
+print(json.dumps(result.json(), indent=4))
--- evergreen.el
+++ evergreen.el
@@ -22,6 +22,9 @@
     )
   )
 
+(define-button-type 'evergreen-patch-button
+  'action (lambda (b) (message "button pressed")))
+
 (defun evergreen-status (project-name)
   "Open the evergreen status page for the given project"
   (let (status-buffer)
@@ -32,16 +35,48 @@
     (insert (format "Project: %s" project-name))
     (evergreen-mode)
     )
-  (make-local-variable 'evergreen-project-name)
-  (make-local-variable 'evergreen-recent-patches)
-  (setq evergreen-project-name project-name)
-  (setq evergreen-recent-patches (evergreen-list-patches))
+  (set (make-local-variable 'evergreen-project-name) project-name)
+  (set (make-local-variable 'evergreen-interface-index) 0)
+  (set (make-local-variable 'evergreen-user) (getenv "EVG_API_USER"))
+  (set (make-local-variable 'evergreen-api-key) (getenv "EVG_API_KEY"))
+  (set (make-local-variable 'evergreen-recent-patches) (evergreen-list-patches))
+
   (newline 2)
   (insert (format "Ongoing Patches (%d):" (length evergreen-recent-patches)))
   (newline)
-  (seq-do (lambda (patch) (insert (format "  %s\n" (alist-get 'description patch)))) evergreen-recent-patches)
+  (set (make-local-variable 'evergreen-interface)
+        (seq-map (lambda (patch)
+                  (insert "  ")
+                  (let ((button
+                         (insert-text-button
+                          (format "%s" (alist-get 'description patch)) :type 'evergreen-patch-button)
+                         ))
+                    (newline)
+                    button)
+                  )
+                evergreen-recent-patches))
   (read-only-mode)
+  (evergreen-modify-interface-index 0)
   (message evergreen-project-name))
+
+(defun evergreen-modify-interface-index (count)
+  (interactive)
+  (let ((newcount (+ count evergreen-interface-index)))
+    (unless (or (>= newcount (length evergreen-interface)) (< newcount 0))
+      (setq evergreen-interface-index newcount)
+      (goto-char (button-start (nth evergreen-interface-index evergreen-interface)))
+      )
+    )
+  )
+
+(defun evergreen-configure-patch (patch)
+  (message "%S" (evergreen-get-patch-variants (alist-get "patch_id" patch)))
+  )
+
+(defun evergreen-view-patch (patch)
+  (if (string= "created" (alist-get ("status" patch)))
+      (evergreen-configure-patch patch))
+  )
 
 (defun evergreen-status-debug ()
   (interactive)
@@ -49,9 +84,7 @@
 
 (defun evergreen-debug ()
   (interactive)
-  (read-only-mode -1)
-  (insert "patch")
-  (read-only-mode)
+  (evergreen-get-patch-variants "5f98b24fd6d80a586d0d1288")
   )
 
 (defvar evergreen-mode-map nil "Keymap for evergreen-status page")
@@ -59,7 +92,15 @@
 (progn
   (setq evergreen-mode-map (make-sparse-keymap))
 
-  (define-key evergreen-mode-map (kbd "p") 'evergreen-patch))
+  (define-key evergreen-mode-map (kbd "p") 'evergreen-patch)
+  (define-key evergreen-mode-map (kbd "d") 'evergreen-debug)
+  (define-key evergreen-mode-map (kbd "r") 'evergreen-status-debug)
+
+  (define-key evergreen-mode-map (kbd "h") 'backward-char)
+  (define-key evergreen-mode-map (kbd "j") (lambda () (interactive) (evergreen-modify-interface-index 1)))
+  (define-key evergreen-mode-map (kbd "k") (lambda () (interactive) (evergreen-modify-interface-index -1)))
+  (define-key evergreen-mode-map (kbd "l") 'forward-char)
+  )
 
 (define-derived-mode
   evergreen-mode
@@ -72,21 +113,52 @@
 (require 'request)
 (require 'seq)
 
+(defun evergreen-get (url &optional params)
+  "Perform a blocking GET request against the given URL"
+  (request-response-data (request
+    url
+    :headers (list (cons "Api-User" evergreen-user) (cons "Api-Key" evergreen-api-key))
+    :params params
+    :sync t
+    :parser 'json-read))
+  )
+
 (defun evergreen-list-patches ()
   "Fetch list of incomplete patches recently submitted by the user."
   (interactive)
   (message "listing patches for %s" evergreen-project-name)
-  (let ((user (getenv "EVG_API_USER")) response)
-    (setq response
-          (request
-            (format "https://evergreen.mongodb.com/api/rest/v2/projects/%s/patches" evergreen-project-name)
-            :headers (list (cons "Api-User" user) (cons "Api-Key" (getenv "EVG_API_KEY")))
-            :params '(("limit" . 5))
-            :sync t
-            :parser 'json-read))
-    (seq-filter
-     (lambda (patch)
-       (and
-        (string= user (alist-get 'author patch))
-        (not (alist-get 'finish_time patch))))
-     (request-response-data response))))
+  (seq-filter
+   (lambda (patch)
+     (and
+      (string= evergreen-user (alist-get 'author patch))
+      (not (alist-get 'finish_time patch))))
+   (evergreen-get
+    (format "https://evergreen.mongodb.com/api/rest/v2/projects/%s/patches" evergreen-project-name)
+    '(("limit" . 5))
+    )
+   )
+  )
+
+(defun evergreen-get-patch (patch-id)
+  "Fetch the details of a single patch as a JSON object."
+  (evergreen-get
+   (format "https://evergreen.mongodb.com/api/rest/v2/patches/%s" patch-id))
+  )
+
+(defun evergreen-get-patch-variants (patch-id)
+  (graphql-request "https://evergreen.mongodb.com/graphql/query"
+                   (format "{ patch(id: \"%s\") { project { variants { displayName,tasks }}}}" patch-id))
+  )
+
+;; From: https://github.com/rcy/graphql-elisp/blob/master/graphql.el
+(defun graphql-request (endpoint query &optional variables)
+  (let* ((url-request-method "POST")
+         (url-request-extra-headers (list (cons "Content-Type"  "application/json") (cons "Api-User" evergreen-user) (cons "Api-Key" evergreen-api-key)))
+         (url-request-data
+          (json-encode (list (cons "query" query)
+                             (cons "variables" (and variables (json-encode variables))))))
+         (buffer (url-retrieve-synchronously endpoint)))
+    (with-current-buffer buffer
+      (goto-char url-http-end-of-headers)
+      (let ((json-object-type 'hash-table))
+        (json-read)))))
