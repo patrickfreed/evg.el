db3b1dfb397fcee674d6c316f4db1ac2891f64d8
Patrick Freed <freedpatrick@gmail.com>
2020-11-27 23:47:39 -0500

    use compilation-mode in view-task buffer

--- evergreen-view-task.el
+++ evergreen-view-task.el
@@ -33,6 +33,8 @@
 (defun evergreen-get-task (task-id)
   (evergreen-task-parse (evergreen-get-p (format "tasks/%s" task-id))))
 
+(defvar evergreen-task-error-regexp-alist '(("mongo-rust-driver" "thread '.*' panicked" "run with `RUST_BACKTRACE=full` for a verbose backtrace")))
+
 (defface evergreen-view-task-title
   '((t
      :bold t
@@ -58,34 +60,47 @@
 (defun evergreen-view-task-refresh ()
   (interactive)
   (evergreen-view-task
-   (evergreen-task-id evergreen-current-task) evergreen-build-variant))
+   (evergreen-task-id evergreen-current-task) evergreen-build-variant evergreen-patch-buffer))
 
 (defun evergreen-view-task-back-to-patch ()
   (interactive)
-  (switch-to-buffer evergreen-task-buffer))
+  (switch-to-buffer evergreen-patch-buffer))
+
+(defun evergreen-view-task-highlight-errors ()
+  "Highlight the error portions of the log output based on provided regex.
+   This code is not used in favor of simply enabling compilation-mode. Kept here if needed
+   at a later date"
+  (pcase (cdr (assoc "mongo-rust-driver" evergreen-task-error-regexp-alist))
+    (`(,start-regexp ,end-regexp) 
+     (while
+         (search-forward-regexp start-regexp nil t)
+       (let* ((start (line-beginning-position)) (end (or (search-forward-regexp end-regexp nil t) (line-end-position))))
+         (set-text-properties start end (list 'face 'diff-removed))
+         )))))
 
 (defun evergreen-view-task (task-id build-variant previous-buffer)
   (switch-to-buffer (get-buffer-create (format "evergreen-view-task: %S" task-id)))
-  (read-only-mode -1)
   (evergreen-view-task-mode)
+  (read-only-mode -1)
   (erase-buffer)
   (setq-local evergreen-build-variant build-variant)
   (setq-local evergreen-current-task (evergreen-get-task task-id))
-  (setq-local evergreen-task-buffer previous-buffer)
+  (setq-local evergreen-patch-buffer previous-buffer)
   (let ((task evergreen-current-task) (build-variant evergreen-build-variant))
     (insert
      (with-temp-buffer
-       (insert (format "%s on %s"
-                       (evergreen-task-display-name task)
-                       build-variant))
+       (insert (format "%s on %s" (evergreen-task-display-name task) build-variant))
        (add-text-properties (point-min) (point-max) (list 'face 'evergreen-view-task-title))
        (buffer-string)))
     (newline)
     (insert (evergreen-view-task-header-line "Status" (evergreen-status-text (evergreen-task-status task))))
     (newline)
     (insert (evergreen-view-task-header-line "Started at" (evergreen-task-start-time task)))
-    (newline 2))
-  (insert (evergreen-get-string (format "%s&text=true" (evergreen-task-task-log evergreen-current-task))))
+    (newline 2)
+    (insert
+     (with-temp-buffer
+       (insert (evergreen-get-string (format "%s&text=true" (evergreen-task-task-log task))))
+       (buffer-string))))
   (read-only-mode)
   (goto-line 0)
   )
@@ -95,7 +110,7 @@
 (progn
   (setq evergreen-view-task-mode-map (make-sparse-keymap))
 
-  (define-key evergreen-view-task-mode-map (kbd "<backtab>") 'evergreen-view-task-back-to-patch)
+  (define-key evergreen-view-task-mode-map (kbd "~") 'evergreen-view-task-back-to-patch)
   (define-key evergreen-view-task-mode-map (kbd "r") 'evergreen-view-task-refresh)
 
   (define-key evergreen-view-task-mode-map (kbd "h") 'backward-char)
@@ -106,6 +121,6 @@
 
 (define-derived-mode
   evergreen-view-task-mode
-  fundamental-mode
+  compilation-mode
   "Evergreen"
   "Major mode for evergreen-view-task buffer")
