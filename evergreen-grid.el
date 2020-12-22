4555b54f754e3c9ce57aa9f0beaf20dd993f0f67
Patrick Freed <freedpatrick@gmail.com>
2020-12-22 15:53:43 -0500

    break grid lines at window edge

--- evergreen-grid.el
+++ evergreen-grid.el
@@ -50,27 +50,27 @@
   (with-temp-buffer
     (seq-do
      (lambda (element)
+       (let ((avg-font-width (ceiling (/ (+ (window-font-width) 2) 2.0)))
+             (max-width (window-width nil t)))
+         ;; give extra column of spacing to ensure line breaks work with buffers of similar size without reloading
+         (when (> (* (current-column) avg-font-width) (- max-width (* 2 (* avg-font-width 2))))
+           (newline)))
        (insert
-        (with-temp-buffer
-          (insert " ")
-          (add-text-properties
-           (point-min)
-           (point-max)
-           (list 'face (evergreen-grid-get-face (evergreen-grid-element-status element))
-                 'help-echo (evergreen-grid-element-description element)
-                 'cursor-sensor-functions (list
-                                           (lambda (w p action)
-                                             (if (eq action 'entered)
-                                                 (message (evergreen-grid-element-description element)))))
-                 'evergreen-element-data (evergreen-grid-element-data element)))
-          (insert
-           (propertize
-            " "
-            'display '(space . (:width (1)))
-            'cursor-intangible t
-            'rear-nonsticky t
-            'front-sticky t
-            ))
-          (buffer-string))))
+        (concat
+         (propertize
+          " "
+          'face (evergreen-grid-get-face (evergreen-grid-element-status element))
+          'help-echo (evergreen-grid-element-description element)
+          'cursor-sensor-functions (list
+                                    (lambda (w p action)
+                                      (if (eq action 'entered)
+                                          (message (evergreen-grid-element-description element)))))
+          'evergreen-element-data (evergreen-grid-element-data element))
+         (propertize
+          " "
+          'display '(space . (:width (1)))
+          'cursor-intangible t
+          'rear-nonsticky t
+          'front-sticky t))))
      elements)
     (buffer-string)))
