a4e1cb484136048f2d3dac93a9a43c0dcc58dbc9
Patrick Freed <patrick.freed@mongodb.com>
2021-02-07 00:42:19 -0500

    fix grid view on terminal emacs

--- evergreen-grid.el
+++ evergreen-grid.el
@@ -6,44 +6,55 @@
 
 (cl-defstruct evergreen-grid-element description status data)
 
-(defun evergreen-grid-success ()
-  `((
-     :background ,(face-attribute 'success :foreground)
-     :foreground "black"
-     :box t)))
+(defface evergreen-grid-rectangle
+  '((t
+     :overline "black"))
+  "Base face for rectangles in an evergreen patch results grid."
+  :group 'evergreen)
 
-(defun evergreen-grid-failed ()
-  `((
-     :background ,(face-attribute 'error :foreground)
-     :foreground "black"
-     :box t)))
-
-(defun evergreen-grid-started ()
-  `((
-     :background ,(face-attribute 'warning :foreground)
-     :foreground "black"
-     :box t)))
-
-(defun evergreen-grid-system-failure ()
-  '((
-     :background "#800080"
-     :foreground "black"
-     :box t)))
-
-(defun evergreen-grid-undispatched ()
-  '((
+(defface evergreen-grid-undispatched
+  '((t
      :background "#bfbfbe"
-     :foreground "black"
-     :box t)))
+     :inherit 'evergreen-grid-rectangle))
+  "Face used for undispatched task results in an evergreen patch results grid."
+  :group 'evergreen)
+
+(defface evergreen-grid-system-failed
+  '((t
+     :background "#800080"
+     :inherit 'evergreen-grid-rectangle))
+  "Face used for tasks that encountered a system failure in an evergreen patch results grid."
+  :group 'evergreen)
+
+(defface evergreen-grid-failed
+  `((t
+     :background ,(face-attribute 'error :foreground)
+     :inherit 'evergreen-grid-rectangle))
+  "Face used for failed task results in an evergreen patch results grid."
+  :group 'evergreen)
+
+(defface evergreen-grid-succeeded
+  `((t
+     :background ,(face-attribute 'success :foreground)
+     :inherit 'evergreen-grid-rectangle))
+  "Face used for successful task results in an evergreen patch results grid."
+  :group 'evergreen)
+
+(defface evergreen-grid-started
+  `((t
+     :background ,(face-attribute 'warning :foreground)
+     :inherit 'evergreen-grid-element))
+  "Face used for tasks that are still executing in an evergreen patch results grid."
+  :group 'evergreen)
 
 (defun evergreen-grid-get-face (status)
   (cond
-   ((string= evergreen-status-success status) (evergreen-grid-success))
-   ((or (string= evergreen-status-failed status) (string= evergreen-status-aborted status)) (evergreen-grid-failed))
-   ((string= evergreen-status-started status) (evergreen-grid-started))
-   ((string= evergreen-status-undispatched status) (evergreen-grid-undispatched))
-   ((string= evergreen-status-system-failure status) (evergreen-grid-system-failure))
-   (t (message "unknown status: %s" status) (evergreen-grid-undispatched))
+   ((string= evergreen-status-success status) 'evergreen-grid-succeeded)
+   ((or (string= evergreen-status-failed status) (string= evergreen-status-aborted status)) 'evergreen-grid-failed)
+   ((string= evergreen-status-started status) 'evergreen-grid-started)
+   ((string= evergreen-status-undispatched status) 'evergreen-grid-undispatched)
+   ((string= evergreen-status-system-failure status) 'evergreen-grid-system-failed)
+   (t (message "unknown status: %s" status) 'evergreen-grid-undispatched)
    ))
 
 (defun evergreen-grid-create (_title elements)
@@ -54,7 +65,13 @@
              (max-width (window-width nil t)))
          ;; give extra column of spacing to ensure line breaks work with buffers of similar size without reloading
          (when (> (* (current-column) avg-font-width) (- max-width (* 2 (* avg-font-width 2))))
-           (newline)))
+           (let ((old-column (current-column)))
+             (newline)
+             ;; need extra line between grid rectangles on terminal since they blend together due to no overline
+             (when (not (display-graphic-p))
+               ;; fill line with spaces so navigating downwards doesn't jump to front of line
+               (insert (make-string (- old-column 1) 32))
+               (newline)))))
        (insert
         (concat
          (propertize
