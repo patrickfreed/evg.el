7e18450c33572648f74cb3738501bf2e38aae9c1
Patrick Freed <patrick.freed@mongodb.com>
2021-04-11 13:47:05 -0400

    fix compiler warnings in evergreen-api

--- src/evergreen-api.el
+++ src/evergreen-api.el
@@ -5,6 +5,13 @@
 
 (provide 'evergreen-api)
 
+(defvar evergreen-user
+  nil
+  "The API user to use to authenticate with the Evergreen API. If unset, this will be read from ~/.evergreen.yml")
+(defvar evergreen-api-key
+  nil
+  "The API key to use to authenticate with the Evergreen API. If unset, this will be read from ~/.evergreen.yml")
+
 (defun evergreen-api-init ()
   "Load credentials from ~/.evergreen.yml if unset.
    This function may be invoked repeatedly, all but the first
