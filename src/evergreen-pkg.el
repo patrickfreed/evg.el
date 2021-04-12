747d5c0bc959ebf59715d991be1ad3a7a72f010d
Patrick Freed <patrick.freed@mongodb.com>
2021-04-11 22:24:24 -0400

    add license

--- src/evergreen-pkg.el
+++ src/evergreen-pkg.el
@@ -1,5 +1,31 @@
+;;; -*- lexical-binding: t -*-
+
+;; Copyright (C) 2021 Patrick Freed
+
+;; Author: Patrick Freed
+;; URL: https://github.com/patrickfreed/evergreen-emacs
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;; A frontend for the Evergreen CI system, implemented as an Emacs package.
+
+;;; Code:
+
 (define-package
   "evergreen"
   "0.1.0"
-  "Emacs frontend to the Evergreen CI system"
+  "A frontend for the Evergreen CI system, implemented as an Emacs package."
   '((request "0.3.2")))
